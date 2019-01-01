734c56576cf6f672c5050e5fd4988db6df0bcc71
Dixzz <vishusatyam36@gmail.com>
2019-01-01 22:44:07 +0530

    Few changes

--- msm8953.mk
+++ msm8953.mk
@@ -304,7 +304,8 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     libxml2 \
     telephony-ext \
-    ims-ext-common
+    ims-ext-common \
+    qti-telephony-common
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
